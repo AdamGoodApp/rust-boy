c40057ec1bec9fee99fecc234ede3c1778441e29
adamgoodapp <raisadamo@gmail.com>
2021-06-03 17:53:32 +0900

    Add cpu impl and Add instruction

--- src/main.rs
+++ src/main.rs
@@ -61,6 +61,43 @@ impl std::convert::From<u8> for FlagsRegister {
     }
 }
 
+// ADD Instruction
+enum Instruction {
+    ADD(ArithmeticTarget),
+}
+  
+  enum ArithmeticTarget {
+    A, B, C, D, E, H, L,
+}
+
+struct CPU {}
+
+impl CPU {
+    fn execute(&mut self, instruction: Instruction) {
+        match instruction {
+        Instruction::ADD(target) => {
+            match target {
+            ArithmeticTarget::C => {
+                let value = self.registers.c;
+                let new_value = self.add(value);
+                self.registers.a = new_value;
+            }
+            _ => { /* TODO: support more targets */ }
+            }
+        }
+        _ => { /* TODO: support more instructions */ }
+        }
+    }
+
+    fn add(&mut self, value: u8) -> u8 {
+        let (new_value, did_overflow) = self.registers.a.overflowing_add(value);
+        // TODO: set flags
+        new_value
+    }
+}
+
+  
+
 fn main() {
     println!("Hello, world!");
 }
